361f9e245f1ac3dc7a6a13945201feadcdc454aa
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2023-11-07 02:12:13 +0300

    Содана админка сайта и одна модел

--- simple_blog/blog/models.py
+++ simple_blog/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    '''Данные о записях в блоге'''
+    title = models.CharField('Заголовок записи', max_length= 100)
+    description = models.TextField('Текст записи')
+    author = models.CharField('Автор статьи', max_length=100)
+    date = models.DateField('Дата')
+    
+    def __str__(self):
+        return f'{self.title} {self.author}' 
+
+    #class Meta:
+        #verbous_name = 'Запись'
+        #verbous_name_plural = 'Записи'
+
+    
+    
+
+    
