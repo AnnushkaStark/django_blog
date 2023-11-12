09aa1f6ae1e6c704baaa65e2510f64e7bb90521f
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2023-11-12 12:33:08 +0300

    Добалена возможность загрузки изображений

--- simple_blog/blog/models.py
+++ simple_blog/blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     description = models.TextField('Текст записи')
     author = models.CharField('Автор статьи', max_length=100)
     date = models.DateField('Дата')
+    image = models.ImageField('Изображение',upload_to='image/%Y')
     
     def __str__(self):
         return f'{self.title} {self.author}' 
