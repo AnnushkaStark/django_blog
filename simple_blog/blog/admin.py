4dbc8761beb8e18bcdaf0a7f09c3e82e67ff6a64
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2023-11-19 02:47:21 +0300

    реализовано сохранение комментариев к постам в базу данных

--- simple_blog/blog/admin.py
+++ simple_blog/blog/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author')
 
+@admin.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('name', 'text_comment', 'date', 'post')
